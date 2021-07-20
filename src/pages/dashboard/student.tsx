0b2575fb9aaaf23209c58ec169c61a44ab730181
Luis André <luis.oliveira10031992@gmail.com>
2021-07-20 01:53:21 -0300

    Add page dashboard for student

--- src/pages/dashboard/student.tsx
+++ src/pages/dashboard/student.tsx
@@ -1,12 +1,35 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, StackDivider, VStack } from "@chakra-ui/react";
+import React from "react";
 import { Header } from "../../components/Header";
+import ListBook from "../../components/ListBook";
 
 export default function Aluno() {
   return (
     <Flex direction="column" h="100vh">
       <Header nameProfile="aluno" />
 
-      <Heading>Tela Aluno</Heading>
+      <VStack
+        divider={<StackDivider borderColor="gray.300" opacity="20%" />}
+        spacing={4}
+        align="stretch"
+      >
+        <Flex
+          as="header"
+          w="100%"
+          maxWidth={1480}
+          h="5"
+          mx="auto"
+          mt="4"
+          px="6"
+          align="center"
+        >
+          <Heading fontSize={22}>Nome da Escola</Heading>
+        </Flex>
+
+        <Box ml={6}>
+          <ListBook />
+        </Box>
+      </VStack>
     </Flex>
   );
 }
